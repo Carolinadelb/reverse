3ec2aecb200dd7bb1aa3bbdb9e72bbd7ed90618e
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-04 20:11:26 -0400

    enunciado

---
+++ app.js
@@ -0,0 +1,6 @@
+/*1. REVERSE
+Crea una función que invierta el orden de los elementos de un array. 
+Completa este reto de dos maneras: en una, el array invertido es un 
+nuevo array; en la segunda, modifica el mismo array que se proporciona
+ como argumento. Investiga sobre el método reverse. ¿Qué puedes concluir?*/
+ 
